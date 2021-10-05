3687cce9df9f3405e51479c8c34633cee1efabe5
Hafiz Muhammad Ahmad Naeem <hafizmahmadnaeem@gmail.com>
2021-10-05 15:12:20 +0500

    lasdjf

--- lec3ReactComponentsCounterApp/index.js
+++ lec3ReactComponentsCounterApp/index.js
@@ -1,1 +1,22 @@
 console.log("\n--->>> Lec 3 || counter app <<<---\n");
+
+import { useState } from "react";
+
+const [initialValue, setValue] = useState(5);
+
+switch (key) {
+  case 5:
+    console.log("useState with value 5");
+
+    break;
+  case 2:
+    console.log("useState with value 2");
+
+    break;
+
+  default:
+    console.log("no useState value is working");
+    break;
+}
+
+setValue = useState(2);
